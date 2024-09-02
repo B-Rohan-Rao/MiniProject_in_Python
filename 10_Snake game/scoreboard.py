76f98906bdb8885def91cb258ef8e0582ab8d3e5
B-Rohan-Rao <raorohan060@gmail.com>
2024-09-02 21:54:17 +0530

    updates git commit -m updates

--- 10_Snake game/scoreboard.py
+++ 10_Snake game/scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.color("white")
         self.goto(0, 260)
@@ -15,13 +16,22 @@ class Scoreboard(Turtle):
         self.update_score()
 
     def increase_score(self):
-        self.clear()
         self.score += 1
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        with open("data.txt") as data:
+            score = data.read()
+        self.write(f"Score: {self.score}, HIGH SCORE: {score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as d:
+                d.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score()
+
+
+
