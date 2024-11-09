0f5faf4485b996e29605c3adf6078200b1eaaadb
B-Rohan-Rao <raorohan060@gmail.com>
2024-11-10 00:02:15 +0530

    interaction with web page

---
+++ 39_Interaction_with_web_page.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(chrome_options)
+driver.get("https://secure-retreat-92358.herokuapp.com")
+
+first_name = driver.find_element(By.NAME, "fName")
+last_name = driver.find_element(By.NAME, "lName")
+email = driver.find_element(By.NAME, "email")
+
+first_name.send_keys("B. Rohan")
+last_name.send_keys("Rao")
+email.send_keys("Rohan@gmail.com", Keys.ENTER)  # This keys.enter functions as pressing enter button.
+
+
+# submit = driver.find_element(By.CSS_SELECTOR, "form button")
+# submit.click()  #This is another way which we can locate the button in the page and click it
